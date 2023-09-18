84f44fe85a0a36d6fe889d9aa963eab6882aa122
KeitaroNagashima <ktaro0512@docomo.ne.jp>
2023-09-18 15:29:00 +0900

    second

---
+++ TrushStation/Garbage.swift
@@ -0,0 +1,19 @@
+//
+//  Garbage.swift
+//  TrushStation
+//
+//  Created by 長島啓太朗 on 2023/09/18.
+//
+
+import Foundation
+import RealmSwift
+
+class Garbage :Object{
+    @Persisted var trushLongtitude :Double = 0
+    @Persisted var trushLatitude :Double = 0
+    @Persisted var number :Int = 0
+    @Persisted var isBurnableGarbage :Bool = false
+    @Persisted var isIncombustibleGarbage :Bool = false
+    @Persisted var isPetBottle :Bool = false
+    @Persisted var isEmptyCan :Bool = false
+}
--- TrushStation/MapViewController.swift
+++ TrushStation/MapViewController.swift
@@ -7,45 +7,37 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 class MapViewController: UIViewController,CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
     @IBOutlet weak var mapKitView: MKMapView!
+    @IBOutlet var modalButton: UIButton!
+    @IBOutlet var positionButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // CLLocationManagerのインスタンス生成
-        var locationManager = CLLocationManager()
-        
-        //位置情報サービスの確認
-        CLLocationManager.locationServicesEnabled()
-        let status = CLLocationManager.authorizationStatus()
-        if(status == CLAuthorizationStatus.notDetermined) {
-            locationManager.requestWhenInUseAuthorization()
-        }
         locationManager.delegate = self
-        //位置情報を使用可能か
-        if CLLocationManager.locationServicesEnabled() {
-
-        //位置情報の取得開始
-          locationManager.startUpdatingLocation()
-
-        }
-        // セキュリティ認証のステータス
-       
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        modalButton.layer.cornerRadius = 35
+        positionButton.layer.cornerRadius = 35
     }
     
     // 位置情報を取得した場合
-    @IBAction func position(){
-        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            guard let newLocation = locations.last else {
-                      return
-                 }
-            let location:CLLocationCoordinate2D
-                        = CLLocationCoordinate2DMake(newLocation.coordinate.latitude, newLocation.coordinate.longitude)
-            print("緯度：", location.latitude, "経度：", location.longitude)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("location: \(location)") // CLLocationManagerクラスで取得した位置情報
+            print("緯度: \(location.coordinate.latitude)")
+            print("経度: \(location.coordinate.longitude)")
         }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           print("error: \(error)")
+       }
+    @IBAction func searchPosition(_ sender: UIButton){
+       locationManager.requestLocation()
       
     }
     
-    
-
 }
 
