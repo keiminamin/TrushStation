80970e94bc5e255f2511af7cbab093e6f8ac7ab3
KeitaroNagashima <ktaro0512@docomo.ne.jp>
2023-09-25 12:07:03 +0900

    third

--- TrushStation/MapViewController.swift
+++ TrushStation/MapViewController.swift
@@ -13,6 +13,9 @@ class MapViewController: UIViewController,CLLocationManagerDelegate {
     @IBOutlet weak var mapKitView: MKMapView!
     @IBOutlet var modalButton: UIButton!
     @IBOutlet var positionButton: UIButton!
+    var latitudeNow: Double = 0
+    var longitudeNow: Double = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
@@ -21,22 +24,52 @@ class MapViewController: UIViewController,CLLocationManagerDelegate {
         locationManager.requestLocation()
         modalButton.layer.cornerRadius = 35
         positionButton.layer.cornerRadius = 35
+        let span : MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region : MKCoordinateRegion = MKCoordinateRegion(center: mapKitView.centerCoordinate, span: span)
+        mapKitView.region = region
+        mapKitView.setCenter(mapKitView.userLocation.coordinate, animated: false)
+        mapKitView.userTrackingMode = .follow
+       
+
     }
     
     // 位置情報を取得した場合
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last {
-            print("location: \(location)") // CLLocationManagerクラスで取得した位置情報
-            print("緯度: \(location.coordinate.latitude)")
-            print("経度: \(location.coordinate.longitude)")
+            if let location = locations.last {
+                print("location: \(location)") // CLLocationManagerクラスで取得した位置情報
+                print("緯度: \(location.coordinate.latitude)")
+                print("経度: \(location.coordinate.longitude)")
+            }
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        // 位置情報を格納する
+        self.latitudeNow = Double(latitude!)
+        self.longitudeNow = Double(longitude!)
         }
-    }
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
            print("error: \(error)")
        }
     @IBAction func searchPosition(_ sender: UIButton){
        locationManager.requestLocation()
       
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let status = CLLocationManager.authorizationStatus()
+        let next = segue.destination
+        if segue.identifier == "toAddModal"{
+            if status == .authorizedWhenInUse{
+                let saveViewController = segue.destination as! SaveViewController
+               
+                saveViewController.longitudeNow = longitudeNow
+                saveViewController.latitudeNow = latitudeNow
+            }else{
+                print("NO")
+            }
+            if let sheet = next.sheetPresentationController {
+                sheet.detents = [.medium()]
+            }
+        }
     }
     
 }
---
+++ TrushStation/SaveViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SaveViewController.swift
+//  TrushStation
+//
+//  Created by 長島啓太朗 on 2023/09/25.
+//
+
+import UIKit
+import MapKit
+class SaveViewController: UIViewController {
+    @IBOutlet var positionLabel:UILabel!
+    var longitudeNow: Double!
+    var latitudeNow: Double!
+    
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let location = CLLocation(latitude: longitudeNow, longitude: latitudeNow)
+        print("OK")
+        CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+            guard let placemark = placemarks?.first, error == nil else { return }
+            print(placemark.name)
+            self.positionLabel.text = placemark.name
+            // あとは煮るなり焼くなり
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
